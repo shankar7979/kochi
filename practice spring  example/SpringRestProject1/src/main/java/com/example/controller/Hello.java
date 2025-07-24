8b42e0436e159eb097f2372a88b0a9737c62299c
shankar7979 <shankar7979@hotmail.com>
2025-07-24 21:17:15 +0530

    added

---
+++ practice spring  example/SpringRestProject1/src/main/java/com/example/controller/Hello.java
@@ -0,0 +1,18 @@
+package com.example.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by shankar on 4/14/2023.
+ */
+//http://localhost:8080/SpringRest/hello/hi
+@RestController
+@RequestMapping("/hello")
+public class Hello {
+
+    @RequestMapping("/hi")
+    public String hello(){
+        System.out.println("hi hello called");
+        return "hello world";
+    }
+}
